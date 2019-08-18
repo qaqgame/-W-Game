baf7fa2fc442a08b11a2202e37315ade81e8c2c6
glodxy <772058363@qq.com>
2019-08-18 18:27:02 +0800

    添加多人互动,修复网络通信
    添加多人操控；
    修复lockstep框架；

--- Assets/hero_shared/script/BasicRoleControll.cs
+++ Assets/hero_shared/script/BasicRoleControll.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BasicRoleControll : MonoBehaviour
 {
-    public Camera camera;
+    
     public int speed=400;
     public float speedInfluence=0.05f;
     public GameObject realObject;
@@ -22,42 +22,24 @@ public class BasicRoleControll : MonoBehaviour
     {
         if(speed<0)
             speed=0;
+        // //跳舞健
+        // if(Input.GetKey(KeyCode.Alpha1)){
+        //     animator.SetInteger("state",2);
+        //     animator.SetInteger("dance_num",1);
+        //     Debug.Log("dance 1");
+        // }
+        // if(Input.GetKey(KeyCode.Alpha2)){
+        //     animator.SetInteger("state",2);
+        //     animator.SetInteger("dance_num",2);
+        // }
+        // if(Input.GetKey(KeyCode.Alpha3)){
+        //     animator.SetInteger("state",2);
+        //     animator.SetInteger("dance_num",3);
+        // }
 
-        if (Input.GetMouseButtonDown(1))//当点击鼠标左键时（左键为0，右键为1）
-        {
-            Ray ray = camera.ScreenPointToRay(Input.mousePosition);//由摄像机向鼠标位置发射射线，Input.mousePosition为鼠标点击的位置 
-            RaycastHit hit;
-        	if(Physics.Raycast(ray, out hit,1000))//Physics.Raycast()表示当射线（ray）与任何碰撞体发生接触时返回true，否则返回false
-        	{
-				if (hit.collider.gameObject.tag == "Plane")//当射线碰撞到的是Plane（此if语句限制鼠标点击位置在Plane上有效）
-            	{
-                    Vector3 targetPosition=hit.point;
-                    targetPosition.y=transform.position.y;
-                    if(LockStepController.Instance.running){
-                        LockStepController.Instance.SendAction(new Move(this.transform.name,0,targetPosition));          
-                    }
-            		
-            	}
-			}   
-        }
-        //跳舞健
-        if(Input.GetKey(KeyCode.Alpha1)){
-            animator.SetInteger("state",2);
-            animator.SetInteger("dance_num",1);
-            Debug.Log("dance 1");
-        }
-        if(Input.GetKey(KeyCode.Alpha2)){
-            animator.SetInteger("state",2);
-            animator.SetInteger("dance_num",2);
-        }
-        if(Input.GetKey(KeyCode.Alpha3)){
-            animator.SetInteger("state",2);
-            animator.SetInteger("dance_num",3);
-        }
-
-        if(Input.GetKey(KeyCode.S)&&((animator.GetInteger("state")==1)||animator.GetInteger("state")==2)){
-            animator.SetInteger("state",0);
-        }
+        // if(Input.GetKey(KeyCode.S)&&((animator.GetInteger("state")==1)||animator.GetInteger("state")==2)){
+        //     animator.SetInteger("state",0);
+        // }
         #region for debug
             Debug.DrawLine(transform.position,endPosition,Color.green);
         #endregion
---
+++ Assets/hero_shared/script/MainRoleController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainRoleController : MonoBehaviour
+{
+    public Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))//当点击鼠标左键时（左键为0，右键为1）
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);//由摄像机向鼠标位置发射射线，Input.mousePosition为鼠标点击的位置 
+            RaycastHit hit;
+        	if(Physics.Raycast(ray, out hit,1000))//Physics.Raycast()表示当射线（ray）与任何碰撞体发生接触时返回true，否则返回false
+        	{
+				if (hit.collider.gameObject.tag == "Plane")//当射线碰撞到的是Plane（此if语句限制鼠标点击位置在Plane上有效）
+            	{
+                    Vector3 targetPosition=hit.point;
+                    targetPosition.y=transform.position.y;
+                    if(LockStepController.Instance.running){
+                        LockStepController.Instance.SendAction(new Move(this.transform.name,0,targetPosition));          
+                    }
+            		
+            	}
+			}   
+        }
+    }
+}
